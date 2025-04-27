694adea4c44ae2199fa9fc79f29ea3c2b45fa568
Anamika Aggarwal <150609132+Anamika1608@users.noreply.github.com>
2025-04-27 14:09:47 +0530

    update margin for unique features heading

--- Client/src/components/Unique/Heading.jsx
+++ Client/src/components/Unique/Heading.jsx
@@ -5,7 +5,7 @@ export function FlipWordsDemo() {
   const words = ["Unique","Key","Smart"];
 
   return (
-    <div className="px-4 my-5">
+    <div className="px-4 my-2">
       <div className="text-4xl mx-auto font-medium text-neutral-600 dark:text-neutral-400">
         <FlipWords words={words} />  Features
       </div>
